2d0ec5caa69e895a15a3f2a13c43c3e49fb6135f
Zoé Jonatan Tapia Hernández <ztapia@bstmexico.com>
2018-02-23 14:07:10 -0600

    Primeros Pasos
    Estos son mis primeros en Xamarin

---
+++ lance/lance/Infraestructure/InstanceLocator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lance.Infraestructure
+{
+    using ViewModels;
+    class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main { get; set; }
+        #endregion
+
+        #region Constructor
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
