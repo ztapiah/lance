bae5164da81efe5f9d5c0364fa50328d6a7a61d5
Zoé Jonatan Tapia Hernández <ztapia@bstmexico.com>
2018-02-27 13:48:15 -0600

    Login View Model

---
+++ lance/lance/ViewModels/LoginViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace lance.ViewModels
+{
+    public class LoginViewModel
+    {
+        #region Properties
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsRunning { get; set; }
+        public bool IsRemembered { get; set; }
+        #endregion
+
+        #region Constructors
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+        }
+        #endregion
+
+        #region Commands
+        public ICommand AceptarCommand { get; set; }
+        #endregion
+    }
+}
--- lance/lance/ViewModels/MainViewModel.cs
+++ lance/lance/ViewModels/MainViewModel.cs
@@ -6,5 +6,15 @@ namespace lance.ViewModels
 {
     public class MainViewModel
     {
+        #region ViewModels
+        public LoginViewModel Login { get; set; }
+        #endregion
+
+        #region Constructor
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
     }
 }
